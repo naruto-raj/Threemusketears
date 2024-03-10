209255111514f00c3813e250ed22e348fd365785
Aditya Sharma <aatom098@gmail.com>
2024-03-10 06:40:35 +0000

    feat: first working commit

--- flask_video_loop/app.py
+++ flask_video_loop/app.py
@@ -2,24 +2,26 @@ from flask import Flask, render_template, request
 import sys, os
 sys.path.append(os.getcwd())
 from llm_video_gen import *
+from markupsafe import Markup
 app = Flask(__name__)
 chain1,chain2 = lang_chains()
 
 def story_gen(text_prompt, chain1, chain2):
     image_prompt, story = LLM_chat(text_prompt,chain1,chain2)
-    print(story)
+    print(f"story:{story}")
     image_prompt = extract_prompt(image_prompt)
+    print(f"image prompt: {image_prompt}")
     image_data = text_to_image(api_key, image_prompt,debug=True)
     # run_typing_effect(story)
     video_data = image_to_video(api_key, image_data,debug=True)
     base64_video_data = base64.b64encode(video_data).decode('utf-8')
-    return base64_video_data
+    return base64_video_data, story
 
 @app.route('/')
 def index():
     # Initially, set the default video URL
     video_url = 'sample_video.mp4'
-    return render_template('index.html', video_url=video_url)
+    return render_template('index.html', video_url=video_url, initial_text_content = "You are a hungry Knight entering the realm of dragons")
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -33,12 +35,15 @@ def submit():
         print("User input:", text_prompt)
         # prompts and generate image and video with next scene
         #"A hungry Knight enters the realm of dragons"
-        base64_video_data = story_gen(text_prompt, chain1, chain2)
+        base64_video_data, story = story_gen(text_prompt, chain1, chain2)
+        story = story.replace('\n', ' ').replace("'", "").replace('"', '')
         print("Video generated")
         # Based on the user input, choose a different video URL
         # video_url = choose_video_url(text_prompt)  # You need to implement this function
         # print(video_url)
-        return render_template('index.html', base64_video_data=base64_video_data)
+        return render_template('index.html', initial_text_content = story, base64_video_data=base64_video_data)
+        # return render_template('index.html', initial_text_content = story, video_url = 'sample_video.mp4')
+
 
 # def choose_video_url(text_prompt):
 #     # Implement your logic here to choose a video URL based on the user input
